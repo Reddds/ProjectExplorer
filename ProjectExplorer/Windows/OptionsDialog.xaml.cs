5a8f58b3e03c49cb059e73668669e3da0f22d3d1
Denis Karaulnykh <denisvkar@gmail.com>
2016-01-26 16:46:04 +0300

    Редактирование тэгов теперь безопасно и можно отменить

--- ProjectExplorer/Windows/OptionsDialog.xaml.cs
+++ ProjectExplorer/Windows/OptionsDialog.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using ProjectExplorer.Models;
+using Xceed.Wpf.DataGrid;
 
 namespace ProjectExplorer.Windows
 {
@@ -10,19 +16,87 @@ namespace ProjectExplorer.Windows
     /// </summary>
     public partial class OptionsDialog
     {
+        private List<ProjectCollectionTag> _original;
+        private List<ProjectCollectionTag> _localCopy;
+
         public OptionsDialog()
         {
             InitializeComponent();
         }
 
-        public void SetTags(IEnumerable<ProjectCollectionTag> tags)
+        public void SetTags(List<ProjectCollectionTag> tags)
         {
-            DgTags.ItemsSource = tags;
+            _original = tags;
+            _localCopy = tags.Select(tag => new ProjectCollectionTag
+            {
+                Id = tag.Id, Name = tag.Name, Color = tag.Color
+            }).ToList();
+            
+            DgTags.ItemsSource = _localCopy;
         }
 
         private void OkClick(object sender, RoutedEventArgs e)
         {
+            // Удаляем удалённые тэги
+            for (var i = _original.Count - 1; i >= 0; i--)
+            {
+                var origTag = _original[i];
+                if (_localCopy.All(t => t.Id != origTag.Id))
+                {
+                    _original.Remove(origTag);
+                }
+            }
+            // Заменяем или добавляем новые тэги
+            foreach (var tag in _localCopy)
+            {
+                var found = _original.FirstOrDefault(t => t.Id == tag.Id);
+                if (found == null)
+                {
+                    found = new ProjectCollectionTag();
+                    _original.Add(found);
+                }
+                found.Name = tag.Name;
+                found.Color = tag.Color;
+            }
             DialogResult = true;
+        }
+
+        private void DgTags_OnRowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            var dg = (DataGrid)sender;
+
+            var curRow = e.Row.DataContext as ProjectCollectionTag;
+            if (curRow == null)
+                return;
+
+/*
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                if (curRow.Id == 0)
+                {
+                    var tags = (List<ProjectCollectionTag>)dg.ItemsSource;
+                    curRow.Id = tags.Max(t => t.Id) + 1;
+
+                }
+            }
+*/
+        }
+
+        private void DgTags_OnBeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            var dg = (DataGrid)sender;
+
+            var curRow = e.Row.DataContext as ProjectCollectionTag;
+
+
+            if (curRow?.Id == 0)
+            {
+                var tags = (List<ProjectCollectionTag>)dg.ItemsSource;
+                curRow.Id = tags.Max(t => t.Id) + 1;
+
+            }
+
+
         }
     }
 }
